810c4efcdf6c272a980c20ff3593826118c3b60f
Mark-74 <balduccimarco2007@gmail.com>
2025-02-16 20:57:29 +0100

    added some solutions

---
+++ training/mattoni.cpp
@@ -0,0 +1,29 @@
+// Punti: 100.0
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, m; cin >> n >> m;
+
+    vector<int> piles(n, 0);
+
+    //query 1 : 3 -> 1 0 -1 0 0
+    for(int i=0; i < m; i++){
+        int l, r; cin >> l >> r;
+        piles[--l]++;
+        if(r != n) piles[r]--;
+    }
+
+    vector<int> f(n, 0);
+
+    //calculating final situation
+    f[0] = piles[0];
+    for(int i=1; i< n; i++){
+        f[i] = f[i-1] + piles[i];
+    }
+
+    nth_element(f.begin(), f.begin() + n/2, f.end());
+    cout << f[n/2];
+}
---
+++ training/ois_rmi.cpp
@@ -0,0 +1,64 @@
+// Punti: 100.0
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+int main()
+{
+    int A, B, N, sum;
+    fin >> A >> B >> N;
+    sum = A+B;
+
+    vector<int> classifica(N);
+    vector<int> punti(N);
+
+    for(int i = 0; i < N; i++){
+        fin >> classifica[i];
+    }
+
+    for(int i = 0; i < N; i++){
+        fin >> punti[i];
+    }
+
+    sort(classifica.begin(), classifica.end());
+    sort(punti.begin(), punti.end());
+
+    int offset_right = 1, offset_left = 0, offset = 0;
+
+    //caso migliore
+    for(int i = N-1; i >= 0; i--){
+        if(classifica[i] > sum){
+            offset_right++;
+            offset++;
+            continue;
+        }
+
+        if(classifica[i] + punti[offset_left] > sum){
+            offset_right++;
+        } else {
+            offset_left++;
+        }
+
+        if(classifica[i] + punti[N-offset_right] <= sum)
+            break;
+    }
+
+    fout << offset_right << ' ';
+
+    //caso peggiore
+    offset_right = 1;
+    for(int i = 0; i < N - offset; i++){
+        if(classifica[i] + punti[N-offset_right] > sum){
+            offset_right++;
+        }
+    }
+
+    offset_right += offset;
+    fout << offset_right;
+
+
+    return 0;
+}
